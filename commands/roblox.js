a28e642458b633297db80c65718576efe885ad49
Trooper-Toaster <46659742+Trooper-Toaster@users.noreply.github.com>
2019-03-24 19:31:33 -0400

    Update roblox.js

--- commands/roblox.js
+++ commands/roblox.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const rbx = require("roblox-js");
 let id = "."
-
+let groupname = "."
 exports.run = (bot, message, args) => {
 let person = args[0]
 
@@ -16,11 +16,22 @@ rbx.getIdFromUsername(person).then(foundId => {
 });
 }
             
-getid()            
+            function getgrouprank(){
+                       rbx.getRankNameInGroup(4666488, id).then(grouprank => {
+            const fp = grouprank
+            
+            groupname = ok
+                       });
+            }
+            
+            
+getid() 
+getgrouprank()
             
 let robloxembed = new Discord.RichEmbed()
 .setTitle("Roblox Player Info")
-.addField("Player ID:", id);                              
+.addField("Player ID:", id)   
+.addField("Rank In Group:", groupname);  
 
             message.channel.send(robloxembed);
             
