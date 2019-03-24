b1f75fab01ead3315077c3496d0be6400d72529f
Trooper-Toaster <46659742+Trooper-Toaster@users.noreply.github.com>
2019-03-24 18:18:30 -0400

    Create roblox.js

---
+++ commands/roblox.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const rbx = require("roblox-js");
+
+
+exports.run = (bot, message, args) => {
+message.channel.send(args[1])
+
+}
+
+
+module.exports.help = {
+  
+  name: "rblxlookup"
+  
+}
