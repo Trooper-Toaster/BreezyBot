bf07c6f7c4ae81895f1b80ea095f2d82ea5f8ed7
Trooper-Toaster <46659742+Trooper-Toaster@users.noreply.github.com>
2019-03-24 21:03:45 -0400

    Create roblox.js

---
+++ commands/roblox.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const roblox = require("roblox.js");
+
+exports.run = (bot, message, args) => {
+  
+  let person = args[0]
+  
+  rbx.getIdFromUsername(person).then(foundId => {
+            const Id = foundId
+            message.channel.send(Id)
+            });
+  message.channel.send
+  
+}
+
+
+module.exports.help = {
+  
+  name: "id"
+  
+}
